996c8df3c812c2ae3e9ff943aaaee0448048a294
ANBARASAN <anbarasan15300@gmail.com>
2023-09-29 19:02:04 +0530

    10-taskName

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -46,7 +46,37 @@ const Navbar = () => {
   return (
     <>
     <nav>
-      
+    <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+       
+       
+      </div>
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
     </nav>
     <Log 
      show={show}
